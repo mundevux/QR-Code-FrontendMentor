1a4baae04a55016a20ae7cf28e45720d543d6c9a
Camilo Cuitiva <camiloandres174@gmail.com>
2022-03-31 17:05:10 -0500

    Add Project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+      "./*.{html,js}",
+  ],
+  theme: {
+    colors:{
+      WhitePrimary: 'hsl(0, 0%, 100%)',
+      Lightgray: "hsl(212, 45%, 89%)",
+      Grayishblue: "hsl(220, 15%, 55%)",
+      Darkblue: "hsl(218, 44%, 22%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
